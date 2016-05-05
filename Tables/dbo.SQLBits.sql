c5c797e1275b2fc5e822ee0be58d3aa7d129894b
Steve Jones <steve.jones@red-gate.com>
2016-05-05 13:06:57 +0100

    added demo columns

--- Tables/dbo.SQLBits.sql
+++ Tables/dbo.SQLBits.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[SQLBits]
 (
 [Liverpool] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[SQLBits] [int] NULL
+[SQLBits] [int] NULL,
+[Demotest] [int] NULL
 ) ON [PRIMARY]
 GO
