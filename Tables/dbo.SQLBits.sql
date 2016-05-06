b9d2fa3ce61d69aa6877b1117a4f1e5347f4b159
Steve Jones <steve.jones@red-gate.com>
2016-05-06 12:30:09 +0100

    new column for Friday

--- Tables/dbo.SQLBits.sql
+++ Tables/dbo.SQLBits.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[SQLBits]
 [Liverpool] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [SQLBits] [int] NULL,
 [Demotest] [int] NULL,
-[NewColumn] [numeric] (18, 0) NULL
+[NewColumn] [numeric] (18, 0) NULL,
+[Friday] [bit] NULL
 ) ON [PRIMARY]
 GO
