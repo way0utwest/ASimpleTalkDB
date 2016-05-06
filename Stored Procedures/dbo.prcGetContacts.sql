6ba5f1e866b20f895d2cc6f750234b61e9f7de2d
Jonathan Hickford <jonathan.hickford@red-gate.com>
2016-05-06 11:50:32 +0100

    updates

--- Stored Procedures/dbo.prcGetContacts.sql
+++ Stored Procedures/dbo.prcGetContacts.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -6,24 +7,7 @@ CREATE PROCEDURE [dbo].[prcGetContacts] @contactid INT = NULL
 AS
 SET NOCOUNT on
     IF @contactid IS NOT NULL
-        SELECT  ContactsID
-              , 'SQLRelayFullName' AS ContactFullName
-              , 'datechanged' = ModifiedDate
-              , Email
-              , LinkedIn, Address1, Address2, Address3
-              , CountryCode
-              , 'LogDate' = GETDATE()
+        SELECT  *
             FROM Contacts
-    ELSE
-        SELECT ContactsID,
-                ContactFullName,
-                ModifiedDate,
-                Email,
-                Photo,
-                LinkedIn,Address1, Address2, address3 
-                CountryCode, 'Logdate' = getdate()
-            FROM dbo.Contacts
-            WHERE ContactsID = @contactid
-			AND CountryCode = 'USA'
 
 GO
---
+++ Tables/dbo.SQlinthecity7.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[SQlinthecity7]
+(
+[id] [int] NULL,
+[secondif] [int] NULL
+) ON [PRIMARY]
+GO
