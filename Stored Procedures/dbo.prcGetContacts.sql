a20a814758f3df0608815bafcf9d2cda1c866a8b
Jonathan Hickford <jonathan.hickford@red-gate.com>
2016-05-06 12:49:15 +0100

    test

--- Stored Procedures/dbo.prcGetContacts.sql
+++ Stored Procedures/dbo.prcGetContacts.sql
@@ -7,7 +7,19 @@ CREATE PROCEDURE [dbo].[prcGetContacts] @contactid INT = NULL
 AS
 SET NOCOUNT on
     IF @contactid IS NOT NULL
-        SELECT  *
+        SELECT  ContactsID
+              , ContactFullName
+              , PhoneWork
+              , PhoneMobile
+              , Address1
+              , Address2
+              , Address3
+              , CountryCode
+              , JoiningDate
+              , ModifiedDate
+              , Email
+              , Photo
+              , LinkedIn
             FROM Contacts
 
 GO
