5803ce3d0e7abcc5ef27c70a765c7a5d5ff94451
Steve Jones <sjones@sqlservercentral.com>
2019-09-05 15:05:35 +0100

    commit 5

---
+++ dbcode/Stored Procedures/dbo.GetFour.sql
@@ -0,0 +1,20 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[GetFour]
+ 
+/*
+Description:
+
+Changes:
+Date       Who         Notes
+---------- ---         ---------------------------------------------------
+9/5/2019  DKRSPECTRE\way0u    
+*/
+AS
+BEGIN
+SELECT 4
+RETURN
+END
+GO
